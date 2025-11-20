f2d3a71b3d3c9244729fc19a6505cc35245e2825
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-11-20 15:20:41 +0000

    updates

--- src/Ch04/04_02b/CodeDemo.cpp
+++ src/Ch04/04_02b/CodeDemo.cpp
@@ -16,9 +16,27 @@ int main(){
     std::cin >> operation;
 
     // switch goes here
+    switch(operation) {
+        case '+':
+            result = operand_1 + operand_2;
+            break;
+        case '-':
+            result = operand_1 - operand_2;
+            break;
+        case '*':
+            result = operand_1 * operand_2;
+            break;
+        case '/':
+            result = operand_1 / operand_2;
+            break;
+        default:
+            result = operand_1 + operand_2;
+            break;
+        }
 
     std::cout << "The result is " << result << std::endl;
     
     std::cout << std::endl << std::endl;
+    
     return (0);
 }
