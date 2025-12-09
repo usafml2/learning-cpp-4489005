700db8f0f865c548d3f49ca3f6ead1c530565756
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-12-09 21:30:54 +0000

    comments added

--- src/Ch05/05_03b/records.h
+++ src/Ch05/05_03b/records.h
@@ -1,59 +1,59 @@
-#pragma once
+#pragma once // records.h
 
-#include <vector>
-#include <string>
+#include <vector> // for std::vector
+#include <string> // for std::string
 
 class Student{ // Student class
-private:
-    int id;
-    std::string name;
+private: // data members
+    int id; // student ID
+    std::string name; // student name
 
-public:
-    Student(int the_id, std::string the_name);
-    int get_id() const;
-    std::string get_name() const;
+public: // member functions
+    Student(int the_id, std::string the_name); // constructor
+    int get_id() const; // get student ID
+    std::string get_name() const; // get student name
 };
 
 class Course{ // Course class
-private:
-    int id;
-    std::string name;
-    unsigned char credits;
+private: // data members
+    int id; // course ID
+    std::string name; // course name
+    unsigned char credits; // course credits
 
-public:
-    Course(int the_id, std::string the_name, unsigned char the_credits);
-    int get_id() const;
-    std::string get_name() const;
-    int get_credits() const;
+public: // member functions
+    Course(int the_id, std::string the_name, unsigned char the_credits); // constructor
+    int get_id() const; // get course ID
+    std::string get_name() const; // get course name
+    int get_credits() const; // get course credits
 };
 
 class Grade{ // Grade class
-private:
-    int student_id;
-    int course_id;
-    char grade;
+private: // data members
+    int student_id; // student ID
+    int course_id; // course ID
+    char grade; // letter grade
 
-public:
-    Grade(int sid, int cid, char grd);
-    int get_student_id() const;
-    int get_course_id() const;
-    char get_grade() const;
+public: // member functions
+    Grade(int sid, int cid, char grd); // constructor
+    int get_student_id() const; // get student ID
+    int get_course_id() const; // get course ID
+    char get_grade() const; // get letter grade
 };
 
 class StudentRecords{ // StudentRecords class
-private:
-    std::vector<Student> students;
-    std::vector<Course> courses;
-    std::vector<Grade> grades;
+private: // data members
+    std::vector<Student> students; // vector of Student objects
+    std::vector<Course> courses; // vector of Course objects
+    std::vector<Grade> grades; // vector of Grade objects
 
-    float get_num_grade(char) const;
+    float get_num_grade(char) const; // get numeric grade from letter grade
     
-public:
-    void add_student(int, std::string);
-    void add_course(int, std::string, unsigned char);
-    void add_grade(int, int, char);
+public: // member functions
+    void add_student(int, std::string); // add a student
+    void add_course(int, std::string, unsigned char); // add a course
+    void add_grade(int, int, char); // add a grade
 
-    std::string get_student_name(int) const;
-    unsigned char get_course_credits(int) const;
-    float get_GPA(int) const;
+    std::string get_student_name(int) const; // get student name
+    unsigned char get_course_credits(int) const; // get course credits
+    float get_GPA(int) const; // get GPA for a student
 };
