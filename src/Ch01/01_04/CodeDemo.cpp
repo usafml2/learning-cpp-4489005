4e54162601857105b58638faa258f5ff2289272f
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-01-21 20:25:01 +0000

    Add comments

--- src/Ch01/01_04/CodeDemo.cpp
+++ src/Ch01/01_04/CodeDemo.cpp
@@ -2,15 +2,15 @@
 // Challenge Solution 01_04
 // Console Interaction, by Eduardo Corpeño 
 
-#include <iostream>
-#include <string>
+#include <iostream> // std::cout, std::cin, std::endl, std::flush
+#include <string> // std::string
 
-int main(){
-    std::string str;
-    std::cout << "Enter your name: " << std::flush;
-    std::cin >> str;
-    std::cout << "Nice to meet you, " << str << "!" << std::endl;
+int main(){ // Main function - execution starts here
+    std::string str; // Variable to hold user input
+    std::cout << "Enter your name: " << std::flush; // Prompt user for their name
+    std::cin >> str; // Read user input into variable
+    std::cout << "Nice to meet you, " << str << "!" << std::endl; // Greet the user
 
-    std::cout << std::endl << std::endl;
-    return (0);
+    std::cout << std::endl << std::endl; // Print two new lines for better readability
+    return (0); // Indicate that the program ended successfully
 }
