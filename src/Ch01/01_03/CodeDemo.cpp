c1f6c89d3692bd124dc385456c890c3311d3705a
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-09 13:57:01 +0000

    Added comments

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,7 +7,8 @@
 
 int main(){ // Main function - execution starts here
     std::string str; // str is a variable to hold user input of class string
-    std::cout << "What is your name?" << std::flush; // Prompt user for their name
+    std::cout << "What is your name?" << std::flush; // Prompt user for their name. flush 
+    //is used to ensure the prompt is displayed before waiting for input
     std::cin >> str; // Read user input into variable. Here str is a string used to store the name
     std::cout << "Nice to meet you, " << str << "!" << std::endl; // Greet the user
 
