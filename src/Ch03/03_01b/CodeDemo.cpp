ec05aaa6aee42182af228723e86d8bcaba22d63f
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-17 18:51:32 +0000

    Added comments

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -8,18 +8,19 @@
 enum class cow_purpose {dairy, meat, hide, pet
 }; // The enum class is a scoped enumeration, which means that the enumerators are accessed using the scope resolution operator (::) and are not implicitly converted to integers. This provides better type safety and prevents naming conflicts or namespace pollution.
 
-struct cow{
+struct cow{ // A structure is a user-defined data type that groups related variables of different types together. In this case, we are defining a structure called "cow" that has three members: name, age, and purpose.
    std::string name;
    int age; 
-   cow_purpose purpose;
+   cow_purpose purpose; // The purpose member is of type cow_purpose, which is the enum class we defined earlier. This allows us to specify the purpose of the cow using the enumerators defined in the cow_purpose enum class.
 };
 
 
 int main(){
-    cow my_cow;
-    my_cow.age = 5;
+    cow my_cow; // We can create an instance of the cow structure called "my_cow". This allows us to access the members of the structure and assign values to them.
+    my_cow.age = 5; // We can access the members of the structure using the dot operator (.) and assign values to them. In this case, we are assigning the age of the cow to 5.
     my_cow.name = "Betsy";
-    my_cow.purpose = cow_purpose::dairy;
+    my_cow.purpose = cow_purpose::dairy; // We can also assign the purpose of the cow using the enumerators defined in the cow_purpose enum class. In this case, we are assigning the purpose of the cow to dairy.
+
     std::cout << my_cow.name << " is a type-" << (int) my_cow.purpose << " cow." <<
     std::endl;
     std::cout << my_cow.name << " is " << my_cow.age << " years old." << std::endl;
