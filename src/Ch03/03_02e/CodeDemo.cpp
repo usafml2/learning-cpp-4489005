b5ec0afc182ccc8a2ef14b0424e562f15cb1a524
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-17 19:17:16 +0000

    Added comments

--- src/Ch03/03_02e/CodeDemo.cpp
+++ src/Ch03/03_02e/CodeDemo.cpp
@@ -7,36 +7,34 @@
 
 enum class cow_purpose {dairy, meat, hide, pet};
 
-class cow{
-public:
-    cow(std::string name_i, int age_i, cow_purpose purpose_i){
-        name = name_i;
+class cow{ // This is the class definition, which includes the data members and member functions of the class.
+public: // This is the public section of the class, which includes the member functions that can be accessed from outside the class.
+    cow(std::string name_i, int age_i, cow_purpose purpose_i){ // This is the constructor of the class, which is a special member function that is called when an object of the class is created. It initializes the data members of the class with the values passed as arguments.
+        name = name_i; // This assigns the value of the "name_i" parameter to the "name" data member of the class.
         age = age_i;
         purpose = purpose_i;
     }
-    std::string get_name() const{
+    std::string get_name() const{ // This is a member function that returns the name of the cow. The "const" keyword indicates that this function does not modify the data members of the class.
         return name;
     }
-    int get_age() const{
+    int get_age() const{ // This is a member function that returns the age of the cow. The "const" keyword indicates that this function does not modify the data members of the class.
         return age;
     }
-    cow_purpose get_purpose() const{
+    cow_purpose get_purpose() const{ // This is a member function that returns the purpose of the cow. The "const" keyword indicates that this function does not modify the data members of the class.
         return purpose;
     }
-private:
-    std::string name;
-    int age;
-    cow_purpose purpose;
+private: // This is the private section of the class, which includes the data members that cannot be accessed from outside the class.
+    std::string name; // This is a data member that stores the name of the cow.
+    int age; // This is a data member that stores the age of the cow.
+    cow_purpose purpose; // This is a data member that stores the purpose of the cow, which is of type "cow_purpose", an enumeration defined above.
 };
 
 int main(){
-    cow my_cow;
-    my_cow.age = 5;
-    my_cow.name = "Betsy";
-    my_cow.purpose = cow_purpose::dairy;
-    std::cout << my_cow.name << " is a type-" << (int) my_cow.purpose << " cow." << std::endl;
-    std::cout << my_cow.name << " is " << my_cow.age << " years old." << std::endl;
-    
+    cow my_cow("Hildy", 7, cow_purpose::pet); // This creates an object of the "cow" class named "my_cow" and initializes it with the name "Hildy", age 7, and purpose "pet".
+
+    std::cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << std::endl;
+    std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
