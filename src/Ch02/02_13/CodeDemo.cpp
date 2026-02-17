9f665df532ea76b95a28df4bb2b2c8354e16b8df
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-17 17:58:29 +0000

    Added comments and modified for understanding

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -9,8 +9,10 @@ int main(){
     int nums[5] = {1,23,32,24,337};
     float result; 
 
-    //Wrie your code here
+    //Write your code here
     int n = sizeof(nums) / sizeof(nums[0]); // Calculate array size
+    //by dividing the array size by the size of a single element.
+
     float sum = 0.0;
     
     for (int i = 0; i < n; ++i) {
@@ -20,7 +22,9 @@ int main(){
     result = sum / n;
     
     std::cout << "The average is " << result << std::endl;
-    
+    std::cout << "The size of nums is " << sizeof(nums) << " in bytes" << std::endl;
+    std::cout << "The size of each element is " << sizeof(nums[0]) << " in bytes" << std::endl;
+    std::cout << "The number of elements is " << n << " in total element count"<< std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
