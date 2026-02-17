e9e062777610af308b791ca38a5fb4da07ac3540
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-17 20:20:23 +0000

    Added comments

--- src/Ch03/03_05e/cow.cpp
+++ src/Ch03/03_05e/cow.cpp
@@ -1,17 +1,17 @@
-#include "cow.h"
+#include "cow.h" // Include the header file for the cow class to implement its member functions
 
-cow::cow(std::string name_i, int age_i, cow_purpose purpose_i){
-    name = name_i;
-    age = age_i;
-    purpose = purpose_i;
+cow::cow(std::string name_i, int age_i, cow_purpose purpose_i){ // Constructor for the cow class that initializes the name, age, and purpose of the cow using the provided parameters
+    name = name_i; // Initialize the name of the cow with the value passed as name_i
+    age = age_i; // Initialize the age of the cow with the value passed as age_i
+    purpose = purpose_i; // Initialize the purpose of the cow with the value passed as purpose_i
 }
-std::string cow::get_name() const{
+std::string cow::get_name() const{ // Getter function to retrieve the name of the cow, marked as const to indicate it does not modify the object
     return name;
 }
-int cow::get_age() const{
+int cow::get_age() const{ // Getter function to retrieve the age of the cow, marked as const to indicate it does not modify the object
     return age;
 }
-cow_purpose cow::get_purpose() const{
+cow_purpose cow::get_purpose() const{ // Getter function to retrieve the purpose of the cow, marked as const to indicate it does not modify the object
     return purpose;
 }
 void cow::set_age(int new_age){
