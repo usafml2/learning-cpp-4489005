a704698f32cae4bd87d3632fdf9aa0a647767d04
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-20 19:27:30 +0000

    Added comments

--- src/Ch03/03_11/CodeDemo.cpp
+++ src/Ch03/03_11/CodeDemo.cpp
@@ -2,19 +2,19 @@
 // Challenge 03_11
 // Create some classes, by Eduardo Corpeño 
 
-#include <iostream>
-#include "records.h"
+#include <iostream> // for std::cout and std::endl
+#include "records.h" // for the Student, Course, and Grade classes. We need to include the records.h header to use the Student, Course, and Grade classes that we defined in that header file.
 
 int main(){
-    Student my_student(1, "Hilda Jones");
-    Course my_course(7, "Physics 101", 4);
-    Grade my_grade(1, 7, 'B');
+    Student my_student(1, "Hilda Jones"); // constructor. Create an object of type Student. my_student is the name of the object. 1 and "Hilda Jones" are the arguments passed to the constructor.
+    Course my_course(7, "Physics 101", 4); // constructor. Create an object of type Course. my_course is the name of the object. 7, "Physics 101", and 4 are the arguments passed to the constructor.
+    Grade my_grade(1, 7, 'B'); // constructor. Create an object of type Grade. my_grade is the name of the object. 1, 7, and 'B' are the arguments passed to the constructor.
 
-    std::cout << "Student: " << my_student.get_name() << std::endl;
-    std::cout << "Course: " << my_course.get_name() << std::endl;
-    std::cout << "Credits: " << my_course.get_credits() << std::endl;
-    std::cout << "Grade: " << my_grade.get_grade() << std::endl;
-    
+    std::cout << "Student: " << my_student.get_name() << std::endl; // std::cout is the standard output stream. We use it to print output to the console. The << operator is used to insert data into the output stream. "Student: " is a string literal that we want to print. my_student.get_name() is a function call that returns the name of the student, which we also want to print. std::endl is used to insert a newline character and flush the output buffer.
+    std::cout << "Course: " << my_course.get_name() << std::endl; // std::cout is the standard output stream. We use it to print output to the console. The << operator is used to insert data into the output stream. "Course: " is a string literal that we want to print. my_course.get_name() is a function call that returns the name of the course, which we also want to print. std::endl is used to insert a newline character and flush the output buffer.
+    std::cout << "Credits: " << my_course.get_credits() << std::endl; // std::cout is the standard output stream. We use it to print output to the console. The << operator is used to insert data into the output stream. "Credits: " is a string literal that we want to print. my_course.get_credits() is a function call that returns the number of credits for the course, which we also want to print. std::endl is used to insert a newline character and flush the output buffer.
+    std::cout << "Grade: " << my_grade.get_grade() << std::endl; // std::cout is the standard output stream. We use it to print output to the console. The << operator is used to insert data into the output stream. "Grade: " is a string literal that we want to print. my_grade.get_grade() is a function call that returns the grade for the student in the course, which we also want to print. std::endl is used to insert a newline character and flush the output buffer.
+
     std::cout << std::endl << std::endl;
     return (0);
 }
--- src/Ch03/03_11/records.h
+++ src/Ch03/03_11/records.h
@@ -1,16 +1,19 @@
-#pragma once
+#pragma once // This is a header guard to prevent multiple inclusions of this header file.
+// It ensures that the contents of this header file are only included once in a single 
+// compilation unit, which can help prevent issues with multiple definitions and
+// improve compilation times.
 
-#include <string>
+#include <string> // for std::string. We need to include the string header to use the std::string class, which we will use to store the names of students and courses.
 
-class Student{
-private:
-    int id;
-    std::string name;
+class Student{ // This is the declaration of the Student class. A class is a user-defined type that can contain data members (variables) and member functions (methods). The Student class will have data members for the student's ID and name, and member functions to access those data members.
+private: // The private access specifier means that the members declared after it are only accessible from within the class. This is a common practice to encapsulate the data and provide controlled access through public member functions.
+    int id; // The id data member will store the student's ID, and the name data member will store the student's name. We use int to store the ID because it is a simple and efficient way to represent numeric identifiers.
+    std::string name; // The id data member will store the student's ID, and the name data member will store the student's name. We use std::string to store the name because it is a convenient and efficient way to handle strings in C++.
 
-public:
-    Student(int the_id, std::string the_name);
-    int get_id() const;
-    std::string get_name() const;
+public: // The public access specifier means that the members declared after it are accessible from outside the class. This is where we will declare the constructor and the member functions to access the data members.
+    Student(int the_id, std::string the_name); // This is the declaration of the constructor for the Student class. A constructor is a special member function that is called when an object of the class is created. It is used to initialize the data members of the class. The constructor takes two parameters: the_id, which will be used to initialize the id data member, and the_name, which will be used to initialize the name data member.
+    int get_id() const; // This is the declaration of a const member function that returns the student's ID.
+    std::string get_name() const; // This is the declaration of a const member function that returns the student's name.
 };
 
 class Course{
