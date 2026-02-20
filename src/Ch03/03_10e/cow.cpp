f103f35530c1f0cea1dc55409062f00a9a2e7015
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-20 19:19:03 +0000

    Added comments

--- src/Ch03/03_10e/cow.cpp
+++ src/Ch03/03_10e/cow.cpp
@@ -1,19 +1,19 @@
-#include "cow.h"
+#include "cow.h" // include cow class definition
 
-cow::cow(std::string name_i, int age_i, cow_purpose purpose_i){
-    name = name_i;
-    age = age_i;
-    purpose = purpose_i;
+cow::cow(std::string name_i, int age_i, cow_purpose purpose_i){ // constructor implementation
+    name = name_i; // initialize name
+    age = age_i; // initialize age
+    purpose = purpose_i; // initialize purpose
 }
-std::string cow::get_name(){
-    return name;
+std::string cow::get_name(){ // getter for name implementation
+    return name; // return name
 }
-int cow::get_age(){
-    return age;
+int cow::get_age(){ // getter for age implementation
+    return age; // return age
 }
-cow_purpose cow::get_purpose(){
+cow_purpose cow::get_purpose(){ // getter for purpose implementation
     return purpose;
 }
-void cow::set_age(int new_age){
+void cow::set_age(int new_age){ // setter for age implementation
     age = new_age;
 }
