38189b54a2d9dbf6a5910a7838f33ca859eb72e0
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-20 18:48:30 +0000

    Added Comments

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -17,8 +17,8 @@ int main(){
 
     std::cout << "The first cow is " << cattle.begin()->get_name() << std::endl;
     std::cout << "At index 1 we have " << cattle[1].get_name() << std::endl;
-    std::cout << "Next to last is " << prev(cattle.end(), 2)->get_name() << std::endl;
-    std::cout << "The last cow is " << (cattle.end() -1)->get_name() << std::endl;
+    std::cout << "Next to last is " << prev(cattle.end(), 2)->get_name() << std::endl;// prev() is a function that takes an iterator and a number, and returns an iterator that is that many positions before the original iterator. In this case, we want to get the cow that is two positions before the end of the vector, which is the next to last cow.
+    std::cout << "The last cow is " << (cattle.end() -1)->get_name() << std::endl; // end() returns an iterator that points to one past the last element in the vector, so we need to subtract 1 to get the last element.
     
     std::cout << std::endl << std::endl;
     return (0);
