4bb65c8ecf2fc2988aa64b765dd84c54e2089a6f
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-20 19:17:14 +0000

    Added comments

--- src/Ch03/03_10e/cow.h
+++ src/Ch03/03_10e/cow.h
@@ -1,22 +1,22 @@
-#pragma once
+#pragma once // cow.h
 
-#ifndef COW_H
-#define COW_H
-#include <string>
+#ifndef COW_H // include guard
+#define COW_H // include guard
+#include <string> // for std::string
 
-enum class cow_purpose {dairy, meat, hide, pet};
+enum class cow_purpose {dairy, meat, hide, pet}; // enum class for cow purposes
 
-class cow{
-public:
-    cow(std::string name_i, int age_i, cow_purpose purpose_i);
-    std::string get_name();
-    int get_age();
-    cow_purpose get_purpose();
-    void set_age(int new_age);
-private:
-    std::string name;
-    int age;
-    cow_purpose purpose;
+class cow{ // cow class definition
+public: // public members
+    cow(std::string name_i, int age_i, cow_purpose purpose_i); // constructor
+    std::string get_name(); // getter for name
+    int get_age(); // getter for age
+    cow_purpose get_purpose(); // getter for purpose
+    void set_age(int new_age); // setter for age
+private: // private members
+    std::string name; // cow's name
+    int age; // cow's age
+    cow_purpose purpose; // cow's purpose
 };
 
 #endif // COW_H
